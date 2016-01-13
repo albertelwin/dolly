590e6e4742d7b2418ba974ba4a3494c87ac21048
Albert Elwin <dev@albertelw.in>
2016-01-13 00:26:33 +0000

    build.bat now automatically zips up asset.pak with lzma compression

--- build.bat
+++ build.bat
@@ -1,10 +1,17 @@
 @echo off
 
+set COMPILE_AND_RUN_ASSET_PACKER=1
+
 IF NOT EXIST bin mkdir bin
 cd bin
 
-REM cl -MTd -Od -Z7 -nologo -Feasset_packer -EHa- -Gm- -GR- -fp:fast -Oi -WX -W4 -wd4996 -wd4100 -wd4189 -wd4127 -wd4201 -DWIN32=1 -I../lib -I../src ../src/asset_packer.cpp shell32.lib user32.lib gdi32.lib -link
-REM cd ../dat && "../bin/asset_packer.exe" && cd ../bin
+IF %COMPILE_AND_RUN_ASSET_PACKER%==1 (
+	cl -MTd -Od -Z7 -nologo -Feasset_packer -EHa- -Gm- -GR- -fp:fast -Oi -WX -W4 -wd4996 -wd4100 -wd4189 -wd4127 -wd4201 -DWIN32=1 -I../lib -I../src ../src/asset_packer.cpp shell32.lib user32.lib gdi32.lib -link
+	cd ../dat
+	"../bin/asset_packer.exe"
+	"C:\Program Files\7-zip\7z" a -mm=lzma asset.zip asset.pak > NUL:
+	cd ../bin
+)
 
 set COMMON_COMPILER_FLAGS=-s TOTAL_MEMORY=134217728 -std=c++11 -Werror -Wall -Wno-missing-braces -Wno-unused-variable
 
